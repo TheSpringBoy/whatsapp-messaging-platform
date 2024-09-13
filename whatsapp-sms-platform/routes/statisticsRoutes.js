c0234bd2dc463c00e83ac4db815cc972b3113155
TheSpringBoy <aviv.klug@gmail.com>
2024-09-13 14:32:15 +0300

    order by message id

--- whatsapp-sms-platform/routes/statisticsRoutes.js
+++ whatsapp-sms-platform/routes/statisticsRoutes.js
@@ -58,7 +58,7 @@ router.get('/per-message', authController.verifyToken, async (req, res) => {
             FROM messages m
             JOIN groups g ON m.group_id = g.id
             GROUP BY m.message_group_id, m.group_id, g.group_name, m.message_text
-            ORDER BY m.group_id;
+            ORDER BY m.id;
         `);
         res.json(result.rows);
     } catch (error) {
